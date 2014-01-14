8c657c7fe3ab9ed2935ea4f3b2463167060d0443
sebastinantony <arun.antony@live.com>
2014-01-14 19:31:15 +0530

    Initial push

---
+++ Areas/Admin/Controllers/PostController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Blogger.Models;
+
+namespace Blogger.Areas.Admin.Controllers
+{
+    public class PostController : Controller
+    {
+        private BloggerEntities db = new BloggerEntities();
+
+        //
+        // GET: /Admin/Post/
+
+        public ActionResult Index()
+        {
+            var posts = db.Posts.Include(p => p.Category1).Include(p => p.Comment1);
+            return View(posts.ToList());
+        }
+
+        //
+        // GET: /Admin/Post/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Post post = db.Posts.Find(id);
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            return View(post);
+        }
+
+        //
+        // GET: /Admin/Post/Create
+
+        public ActionResult Create()
+        {
+            ViewBag.Category = new SelectList(db.Categories, "Id", "Name");
+            ViewBag.Comment = new SelectList(db.Comments, "Id", "Name");
+            return View();
+        }
+
+        //
+        // POST: /Admin/Post/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Posts.Add(post);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Category = new SelectList(db.Categories, "Id", "Name", post.Category);
+            ViewBag.Comment = new SelectList(db.Comments, "Id", "Name", post.Comment);
+            return View(post);
+        }
+
+        //
+        // GET: /Admin/Post/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Post post = db.Posts.Find(id);
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Category = new SelectList(db.Categories, "Id", "Name", post.Category);
+            ViewBag.Comment = new SelectList(db.Comments, "Id", "Name", post.Comment);
+            return View(post);
+        }
+
+        //
+        // POST: /Admin/Post/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(post).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Category = new SelectList(db.Categories, "Id", "Name", post.Category);
+            ViewBag.Comment = new SelectList(db.Comments, "Id", "Name", post.Comment);
+            return View(post);
+        }
+
+        //
+        // GET: /Admin/Post/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Post post = db.Posts.Find(id);
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+            return View(post);
+        }
+
+        //
+        // POST: /Admin/Post/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Post post = db.Posts.Find(id);
+            db.Posts.Remove(post);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
